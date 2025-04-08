dced08cb8aa4080530912b3a9b970f64935b0d8b
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-08 15:06:29 -0300

    chore: reorganiza pastas

---
+++ src/components/Main/components/Register/Register.jsx
@@ -0,0 +1,27 @@
+import InfoTooltip from "./components/InfoTooltip";
+
+export default function Register({ onClosePopup }) {
+  const infoTooltip = <InfoTooltip onClosePopup={onClosePopup} />;
+
+  return (
+    <div className="register">
+      <h2 className="register__title">Inscrever-se</h2>
+      <form className="register__form">
+        <input
+          className="register__input register__input_margin_s"
+          type="email"
+          placeholder="E-mail"
+        />
+        <input
+          className="register__input register__input_margin_l"
+          type="password"
+          placeholder="Senha"
+        />
+      </form>
+      <button className="register__button">Inscrever-se</button>
+      <p className="register__sign-up-text">
+        Já é um membro? Faça o login aqui!
+      </p>
+    </div>
+  );
+}
