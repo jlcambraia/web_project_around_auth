4b3298f732b974c594294928069cf9fc1a643d57
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-08 14:56:34 -0300

    feat: adiciona componente Login

---
+++ src/components/Main/components/Login.jsx
@@ -0,0 +1,23 @@
+export default function Login() {
+  return (
+    <div className="login">
+      <h2 className="login__title">Entrar</h2>
+      <form className="login__form">
+        <input
+          className="login__input login__input_margin_s"
+          type="email"
+          placeholder="E-mail"
+        />
+        <input
+          className="login__input login__input_margin_l"
+          type="password"
+          placeholder="Senha"
+        />
+      </form>
+      <button className="login__button">Entrar</button>
+      <p className="login__sign-up-text">
+        Ainda não é membro? Inscreva-se aqui!
+      </p>
+    </div>
+  );
+}
