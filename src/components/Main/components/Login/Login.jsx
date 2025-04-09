ef8457cbfacbb96a3f63c78451e5a7e9c26f33dc
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-09 16:36:01 -0300

    feat: adiciona validação de formulários

--- src/components/Main/components/Login/Login.jsx
+++ src/components/Main/components/Login/Login.jsx
@@ -1,46 +1,123 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Login({ handleLogin }) {
   const [data, setData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleChange = (evt) => {
-    const { name, value } = evt.target;
-    setData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const handleEmailChange = (evt) => {
+    const input = evt.target;
+    setData({
+      ...data,
+      email: input.value,
+    });
+
+    setErrors({
+      ...errors,
+      email: input.validationMessage,
+    });
   };
+
+  const handlePasswordChange = (evt) => {
+    const input = evt.target;
+    setData({
+      ...data,
+      password: input.value,
+    });
+
+    setErrors({
+      ...errors,
+      password: input.validationMessage,
+    });
+  };
+
+  useEffect(() => {
+    setIsFormValid(
+      data.email.trim() !== "" &&
+        data.password.trim() !== "" &&
+        !errors.email &&
+        !errors.password
+    );
+  }, [data, errors]);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    handleLogin(data);
+
+    const form = evt.target;
+    if (form.checkValidity()) {
+      setIsSubmitting(true);
+      handleLogin(data).finally(() => {
+        setIsSubmitting(false);
+      });
+    } else {
+      const emailInput = form.elements.email;
+      const passwordInput = form.elements.password;
+
+      setErrors({
+        email: emailInput.validationMessage,
+        password: passwordInput.validationMessage,
+      });
+    }
   };
 
   return (
     <div className="login">
       <h2 className="login__title">Entrar</h2>
-      <form className="login__form" onSubmit={handleSubmit}>
-        <input
-          name="email"
-          className="login__input login__input_margin_s"
-          type="email"
-          placeholder="E-mail"
-          value={data.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          name="password"
-          className="login__input login__input_margin_l"
-          type="password"
-          placeholder="Senha"
-          value={data.password}
-          onChange={handleChange}
-          required
-        />
-        <button className="login__button" type="submit">
-          Entrar
+      <form className="login__form" onSubmit={handleSubmit} noValidate>
+        <div className="login__input-wrapper">
+          <input
+            name="email"
+            className={`login__input login__input_margin_s ${
+              errors.email ? "login__input_type_error" : ""
+            }`}
+            type="email"
+            placeholder="E-mail"
+            required
+            value={data.email}
+            onChange={handleEmailChange}
+          />
+          <span
+            className={`login__input-error ${
+              errors.email ? "" : "login__input-error_hidden"
+            }`}
+          >
+            {errors.email}
+          </span>
+        </div>
+        <div className="login__input-wrapper">
+          <input
+            name="password"
+            className={`login__input login__input_margin_l ${
+              errors.password ? "login__input_type_error" : ""
+            }`}
+            type="password"
+            placeholder="Senha"
+            required
+            minLength="8"
+            value={data.password}
+            onChange={handlePasswordChange}
+          />
+          <span
+            className={`login__input-error login__input-error_positioned-top ${
+              errors.password ? "" : "login__input-error_hidden"
+            }`}
+          >
+            {errors.password}
+          </span>
+        </div>
+        <button
+          className={`login__button ${
+            !isFormValid || isSubmitting ? "login__button_disabled" : ""
+          }`}
+          type="submit"
+          disabled={!isFormValid || isSubmitting}
+        >
+          {isSubmitting ? "Entrando..." : "Entrar"}
         </button>
       </form>
 
