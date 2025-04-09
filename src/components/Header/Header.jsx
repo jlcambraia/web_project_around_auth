2cf84615404a7cd0a16e24b40f075dfed7101022
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-09 14:44:40 -0300

    feat: implementa botões do header e faz com que botão de logout seja funcional

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,16 @@
 import logo from "../../images/logo.svg";
+import { useNavigate } from "react-router-dom";
+import { removeToken } from "../../utils/token";
 
-export default function Header() {
+export default function Header({ isLoggedIn, setIsLoggedIn, title, userData }) {
+  const navigate = useNavigate();
+
+  function signOut() {
+    removeToken();
+    navigate("/signin");
+    setIsLoggedIn(false);
+  }
+
   return (
     <header className="header">
       <img
@@ -8,7 +18,21 @@ export default function Header() {
         alt="Logotipo em branco da Around The U.S."
         className="header__logo"
       />
-      <button className="header__button">Faça o login</button>
+      {isLoggedIn ? (
+        <div>
+          <button className="header__button header__button_profile">
+            {userData}
+          </button>
+          <button
+            onClick={signOut}
+            className="header__button header__button_logout"
+          >
+            Sair
+          </button>
+        </div>
+      ) : (
+        <button className="header__button">{title}</button>
+      )}
     </header>
   );
 }
