cff469cfe3ced716d26c3b750d39be2764832a63
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-09 15:59:20 -0300

    feat: adiciona useLocation e deixa funcional

--- src/components/ProtectedRout.jsx
+++ src/components/ProtectedRout.jsx
@@ -1,8 +1,19 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
-export default function ProtectedRoute({ isLoggedIn, children }) {
-  if (!isLoggedIn) {
-    return <Navigate to="signin" replace />;
+export default function ProtectedRoute({
+  isLoggedIn,
+  children,
+  anonymous = false,
+}) {
+  const location = useLocation();
+  const from = location.state?.from || "/";
+
+  if (anonymous && isLoggedIn) {
+    return <Navigate to={from} />;
+  }
+
+  if (!anonymous && !isLoggedIn) {
+    return <Navigate to="signin" state={{ from: location }} />;
   }
 
   return children;
