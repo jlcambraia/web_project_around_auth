4823475d9be4c23332ba7d80e43dd2863f3e35c9
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-09 13:45:28 -0300

    feat: deixa compoente Register funcional

--- src/components/Main/components/Register/Register.jsx
+++ src/components/Main/components/Register/Register.jsx
@@ -1,25 +1,49 @@
-import InfoTooltip from "./components/InfoTooltip";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export default function Register({ onClosePopup }) {
-  const infoTooltip = <InfoTooltip onClosePopup={onClosePopup} />;
+export default function Register({ handleRegistration }) {
+  const [data, setData] = useState({ email: "", password: "" });
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    handleRegistration(data);
+  };
 
   return (
     <div className="register">
       <h2 className="register__title">Inscrever-se</h2>
-      <form className="register__form">
+      <form className="register__form" onSubmit={handleSubmit}>
         <input
+          name="email"
           className="register__input register__input_margin_s"
           type="email"
           placeholder="E-mail"
+          value={data.email}
+          onChange={handleChange}
+          required
         />
         <input
+          name="password"
           className="register__input register__input_margin_l"
           type="password"
           placeholder="Senha"
+          value={data.password}
+          onChange={handleChange}
+          required
         />
+        <button className="register__button" type="submit">
+          Inscrever-se
+        </button>
       </form>
-      <button className="register__button">Inscrever-se</button>
+
       <p className="register__sign-up-text">
         Já é um membro?{" "}
         <Link to="/signin" className="register__sign-up-link">
