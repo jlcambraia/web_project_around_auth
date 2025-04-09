dc63d0d89c6aaca7d6457e7a201673dca4b1cf7f
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-09 13:45:39 -0300

    feat: deixa componente Login funcional

--- src/components/Main/components/Login/Login.jsx
+++ src/components/Main/components/Login/Login.jsx
@@ -1,22 +1,49 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export default function Login() {
+export default function Login({ handleLogin }) {
+  const [data, setData] = useState({ email: "", password: "" });
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    handleLogin(data);
+  };
+
   return (
     <div className="login">
       <h2 className="login__title">Entrar</h2>
-      <form className="login__form">
+      <form className="login__form" onSubmit={handleSubmit}>
         <input
+          name="email"
           className="login__input login__input_margin_s"
           type="email"
           placeholder="E-mail"
+          value={data.email}
+          onChange={handleChange}
+          required
         />
         <input
+          name="password"
           className="login__input login__input_margin_l"
           type="password"
           placeholder="Senha"
+          value={data.password}
+          onChange={handleChange}
+          required
         />
+        <button className="login__button" type="submit">
+          Entrar
+        </button>
       </form>
-      <button className="login__button">Entrar</button>
+
       <p className="login__sign-up-text">
         Ainda não é membro?{" "}
         <Link to="/signup" className="login__sign-up-link">
