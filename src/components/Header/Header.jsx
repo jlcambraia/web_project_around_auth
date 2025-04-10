c13041654b06d98bdd7b7fde3db0f1efa7789c91
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-10 11:54:29 -0300

    feat: implementa lógica para mostrar menu no mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import logo from "../../images/logo.svg";
+import openMenu from "../../images/header__menu.svg";
+import closeMenu from "../../images/close__icon.svg";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { removeToken } from "../../utils/token";
 
 export default function Header({ isLoggedIn, setIsLoggedIn, title, userData }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const navigate = useNavigate();
 
   function signOut() {
@@ -19,30 +24,63 @@ export default function Header({ isLoggedIn, setIsLoggedIn, title, userData }) {
     return navigate("/signin");
   }
 
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <header className="header">
-      <img
-        src={logo}
-        alt="Logotipo em branco da Around The U.S."
-        className="header__logo"
-      />
-      {isLoggedIn ? (
-        <div>
-          <button className="header__button header__button_profile">
-            {userData}
-          </button>
-          <button
-            onClick={signOut}
-            className="header__button header__button_logout"
-          >
-            Sair
-          </button>
+    <>
+      {isLoggedIn && isMenuOpen && (
+        <div className="header__mobile-menu">
+          <div className="header__mobile-menu-content">
+            <button className="header__button header__button_profile">
+              {userData}
+            </button>
+            <button
+              onClick={signOut}
+              className="header__button header__button_logout"
+            >
+              Sair
+            </button>
+          </div>
         </div>
-      ) : (
-        <button className="header__button" onClick={buttonNavigate}>
-          {title}
-        </button>
       )}
-    </header>
+
+      <header className="header">
+        <img
+          src={logo}
+          alt="Logotipo em branco da Around The U.S."
+          className="header__logo"
+        />
+        {isLoggedIn ? (
+          <>
+            <div className="header__buttons header__buttons_mobile-hidden">
+              <button className="header__button header__button_profile">
+                {userData}
+              </button>
+              <button
+                onClick={signOut}
+                className="header__button header__button_logout"
+              >
+                Sair
+              </button>
+            </div>
+            <div className="header__buttons header__buttons_desktop-hidden">
+              <button className="header__button" onClick={toggleMenu}>
+                <img
+                  className="header__button-icon"
+                  src={isMenuOpen ? closeMenu : openMenu}
+                  alt="Ícone de menu para mobile"
+                />
+              </button>
+            </div>
+          </>
+        ) : (
+          <button className="header__button" onClick={buttonNavigate}>
+            {title}
+          </button>
+        )}
+      </header>
+    </>
   );
 }
